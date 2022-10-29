466d5e330d94e8e4f1975be6cb6ebf86a4b8cc23
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-29 14:27:51 +0530

    dynamically rendered worker details

--- the_list/pages/Workers/[id].js
+++ the_list/pages/Workers/[id].js
@@ -1,7 +1,42 @@
 import React from "react";
 
-const WorkerDetails = () => {
-  return <div>This is the details page</div>;
+export const getStaticPaths = async () => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await response.json();
+
+  const workerPaths = data.map((worker) => {
+    return {
+      params: { id: worker.id.toString() },
+    };
+  });
+
+  return {
+    paths: workerPaths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const data = await response.json();
+
+  return {
+    props: { worker: data },
+  };
+};
+
+const WorkerDetails = ({ worker }) => {
+  return (
+    <div>
+      <h1>{`${worker.name}'s details`}</h1>
+      <p>{worker.email}</p>
+      <p>{worker.website}</p>
+      <p>{worker.address.city}</p>
+    </div>
+  );
 };
 
 export default WorkerDetails;
