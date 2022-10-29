d573fc6ffb2046fcc2aa4885e6d209092bb51bdd
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-29 14:08:58 +0530

    mapped employee data on worker list page

--- the_list/pages/Workers/WorkerList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WorkerList = () => {
-  return <div>WorkerList</div>;
-};
-
-export default WorkerList;
--- the_list/pages/Workers/index.js
+++ the_list/pages/Workers/index.js
@@ -1,7 +1,30 @@
-const Workers = () => {
+import styles from "../../styles/Workers.module.css";
+
+export const getStaticProps = async () => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await response.json();
+
+  return {
+    props: { workers: data },
+  };
+};
+
+const Workers = ({ workers }) => {
+  console.log(workers);
   return (
     <div>
       <h1>All workers listed below</h1>
+      <div className="Workers-container">
+        {workers.map((worker) => {
+          return (
+            <div key={worker.id}>
+              <a className={styles.single}>
+                <h3>{worker.name}</h3>
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
